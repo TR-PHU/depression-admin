0e87060b086d9d44f8c89b8eaebe9f6f15274ca4
Quyen Nguyen <tuquyennguyen234@gmail.com>
2022-06-08 08:34:12 +0700

    nft

--- src/pages/nft/Nft.jsx
+++ src/pages/nft/Nft.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import Datatable from "../../components/post/Datatable";
 import axios from "axios";
+import "./nft.scss";
 
 const Nft = () => {
   const handleChangeImg = async (e) => {
@@ -11,38 +15,41 @@ const Nft = () => {
     const token = localStorage.getItem("token");
 
     try {
-        const res = await axios({
-            method: "POST",
-            url: "https://api.fullstackjourney.club/api/v1/nft/upload",
-            data: formData,
-            headers: {
-            Authorization: `Bearer ${token}`,
-            "content-type": "multipart/form-data",
-            },
-        });
-        alert("Upload nft thành công")
+      const res = await axios({
+        method: "POST",
+        url: "https://api.fullstackjourney.club/api/v1/nft/upload",
+        data: formData,
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "content-type": "multipart/form-data",
+        },
+      });
+      alert("Upload nft thành công");
     } catch (error) {
-        alert(error.message)
+      alert(error.message);
     }
   };
 
   return (
-    <div className="w-auto min-h-full">
-    <div className="w-full h-full flex justify-center items-center flex-col gap-14 py-20">
-      <div className="flex gap-5 items-center">
-        <label className="m-0 p-0 w-auto h-full">Thêm file excel:</label>
-        <div>
-          <input
-            className="w-full"
-            type="file"
-            name="image"
-            multiple
-            onChange={handleChangeImg}
-          />
+    <div className="list">
+      <Sidebar />
+
+      <div className="listContainer">
+        <Navbar />
+        <div className="listGrid">
+          <h2 className="label">Thêm NFTs:</h2>
+          <div>
+            <input
+              type="file"
+              name="image"
+              multiple
+              onChange={handleChangeImg}
+            />
+          </div>
         </div>
+        <Datatable />
       </div>
     </div>
-  </div>
   );
 };
 
